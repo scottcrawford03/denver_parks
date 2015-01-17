2b2e3333be91f9e3ecaf8773ada36fe5eeb21baf
Scott Crawford <scottcrawford03@gmail.com>
2015-01-16 20:09:04 -0700

    psql db set up and imported

--- db/migrate/20150117022615_create_parks.rb
+++ db/migrate/20150117022615_create_parks.rb
@@ -1,34 +1,34 @@
 class CreateParks < ActiveRecord::Migration
   def change
     create_table :parks do |t|
-      t.string  :LOCATION
-      t.string  :FORMAL_NAME
-      t.string  :LOC_CODE
-      t.string  :PARK_TYPE
-      t.string  :PARK_CLASS
-      t.float   :GIS_ACRES
-      t.string  :DESIGNATED
-      t.integer :FIRST_AQ_DATE
-      t.string  :MASTER_PLAN
-      t.string  :MAINT_DIST
-      t.string  :COUNCIL_DIST
-      t.string  :POLICE_DIST
-      t.string  :CROSS_STREETS
-      t.string  :ADDRESS_ID
-      t.string  :ADDRESS_LINE1
-      t.string  :ADDRESS_LINE2
-      t.string  :CITY
-      t.string  :STATE
-      t.string  :ZIP
-      t.float   :LATITUDE
-      t.float   :LONGITUDE
-      t.string  :MARKETING_CODE
-      t.string  :FACILITIES
-      t.string  :DIAGRAM
-      t.string  :PHOTO
-      t.string  :PARCEL_MATCH
-      t.string  :BND_QC
-      t.string  :GLOBALID
+      t.string  :location
+      t.string  :formal_name
+      t.string  :loc_code
+      t.string  :park_type
+      t.string  :park_class
+      t.float   :gis_acres
+      t.string  :designated
+      t.string :first_aq_date
+      t.string  :master_plan
+      t.string  :maint_dist
+      t.string  :council_dist
+      t.string  :police_dist
+      t.string  :cross_streets
+      t.string  :address_id
+      t.string  :address_line1
+      t.string  :address_line2
+      t.string  :city
+      t.string  :state
+      t.string  :zip
+      t.float   :latitude
+      t.float   :longitude
+      t.string  :marketing_code
+      t.string  :facilities
+      t.string  :diagram
+      t.string  :photo
+      t.string  :parcel_match
+      t.string  :bnd_qc
+      t.string  :globalid
     end
   end
 end
