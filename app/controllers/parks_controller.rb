b24f584a2b32747bc9b19f08999c83eae2ec6e68
David Bayendor <dbayendor@gmail.com>
2015-01-18 16:13:30 -0700

    View park details from list
    * Closes #8

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,7 +1,13 @@
 class ParksController < ApplicationController
+  layout 'park_show', only: :show
+
   def index
     @search = Park.search(params[:q])
     @parks = @search.result.page(params[:page])
     @markers = @search.result
+  end
+
+  def show
+    @park = Park.find(params[:id])
   end
 end
