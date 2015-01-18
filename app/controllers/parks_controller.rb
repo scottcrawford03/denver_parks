fcc4ae17b198e29f9e956b111354eff4cbf99afd
Bryce Holcomb <bryce_holcomb@outlook.com>
2015-01-18 12:00:04 -0700

    fix merge conflicts

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -1,10 +1,6 @@
 class ParksController < ApplicationController
   def index
-<<<<<<< HEAD
-    @parks = Park.all
-=======
     @search = Park.search(params[:q])
     @parks = @search.result.page(params[:page])
->>>>>>> map-markers
   end
 end
