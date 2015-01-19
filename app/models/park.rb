170184deee3610f691ff9788e07ac9dfa459f5e4
David Bayendor <dbayendor@gmail.com>
2015-01-19 01:23:13 -0700

    Simplify search with dropdown box
    * Add “Clear” button

--- app/models/park.rb
+++ app/models/park.rb
@@ -1,6 +1,11 @@
 class Park < ActiveRecord::Base
   self.per_page = 10
 
+  FACILITIES = ['Benches', 'Bike/Pedestrian Path', 'Community Garden',
+                'Dog Park', 'Fountain', 'Grills', 'Lake', 'Outdoor Pool',
+                'Parking Lot', 'Picnic Tables', 'Playground', 'Public Art',
+                'Restroom', 'Skate Park', 'Wildlife Habitat']
+
   def self.facility_includes(type)
     where(parks[:facilities].matches("%#{type}%"))
   end
